0e8d670d56c9b0e58d65d5c0c2ce9473b3299c32
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-02-03 16:52:55 -0500

    feature Se cambia el tipo de letra a inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
+  weight: ["400", "500", "600", "700"],
+  display: "swap"
+})
 
 export const metadata: Metadata = {
   title: "Nexo Backoffice",
@@ -26,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}        
+        className={`${inter.variable} antialiased`}
       >
         <Providers>
           {children}

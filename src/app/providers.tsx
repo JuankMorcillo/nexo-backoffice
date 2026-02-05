e4f08959467da8fb6c859128a4c86b6403b29e52
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-02-05 15:13:18 -0500

    feature Se configura proyecto para uso de redux y se empieza a usar en el sidebar

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,13 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
 import { ReactNode } from "react";
+import { store } from "./store";
 
 export function Providers({ children }: { children: ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>;
+
+    return <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+    </Provider>;
 }

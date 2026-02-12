7dc8988a7a8791d101c9c0c01a58780662217d30
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-02-12 16:04:55 -0500

    feature se crea componente de registro de clientes; se hacen mejoras respecto al tipado de las opciones de envio de datos al api; se crea tipado para majera informacion del cliente; Se exportan tipados de table; se recibe el id del suscriptor

--- src/app/types/clients.ts
+++ src/app/types/clients.ts
@@ -4,3 +4,13 @@ export interface FetchClientsPayload {
     token: string;
     params: Params;
 }
+
+export interface Client {
+    id?: number;
+    token?: string;
+    subscribers_id?: number;
+    name: string;
+    nit: string;
+    address: string;
+    phone: string;
+}

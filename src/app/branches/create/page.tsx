56c973a63934dfafb3d12d8f7919ed0ca91c406b
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-02-20 10:06:21 -0500

    feature Se crea componente para crear sucursal

---
+++ src/app/branches/create/page.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from '../../store';
+import { useSession } from 'next-auth/react';
+import { clearProcessMessageBranch, createBranchSlice, selectBranchesLoading, selectBranchesProcessMessage, selectBranchesSuccess, setSuccessBranch } from '../../store/slices/branchesSlice';
+import Iconos from '../../components/ui/hooks/iconos';
+import { Branch } from '../../types/branches';
+import { fillToastInfo } from '../../store/slices/toastSlice';
+import { triggerReload } from '../../store/slices/reloadSlice';
+import Inputs from '../../components/inputs';
+import useClienteInfo from '@/src/hooks/useClienteInfo';
+
+
+export default function CreateBranch() {
+
+    const dispatch = useDispatch<AppDispatch>()
+    const { data: session } = useSession()
+
+    const loading = useSelector(selectBranchesLoading)
+    const message = useSelector(selectBranchesProcessMessage)
+    const success = useSelector(selectBranchesSuccess)
+
+    const { successIcon } = Iconos({ classNames: 'size-6 text-green-500', fill: 'currentColor', stroke: 'currentColor', strokeWidth: 1.5 })
+    const { circleXMarkIcon } = Iconos({ classNames: 'size-6 text-red-500', fill: 'currentColor', stroke: 'currentColor', strokeWidth: 1.5 })
+
+    const { clients } = useClienteInfo()
+
+    const [info, setInfo] = useState<Branch>()
+
+    const inputs: Inputs = [
+        {
+            id: 'name',
+            label: 'Nombre',
+            type: 'text',
+            placeholder: 'Nombre de la sucursal',
+            required: true
+        },
+        {
+            id: 'address',
+            label: 'Dirección',
+            type: 'text',
+            placeholder: 'Dirección de la sucursal',
+            required: true
+        },
+        {
+            id: 'phone',
+            label: 'Teléfono',
+            type: 'text',
+            placeholder: 'Teléfono de la sucursal',
+            required: true
+        },
+        {
+            id: 'clients_id',
+            label: 'Cliente',
+            type: 'select',
+            list: true,
+            required: true,
+            options: clients
+        }
+    ]
+
+    const styles = {
+        cols: 1,
+        textButton: 'Guardar Sucursal',
+    }
+
+    const handleCreateBranch = async () => {
+        if (session?.user.access_token) {
+
+            const result = await dispatch(
+                createBranchSlice({ token: session.user.access_token, branch: info as Branch })
+            )
+
+            if (result.type == 'branches/createBranch/fulfilled') {
+                setInfo(undefined)
+            }
+
+        }
+    }
+
+    useEffect(() => {
+
+        if (message) {
+            dispatch(fillToastInfo({
+                id: new Date().getTime().toString(),
+                message: message || 'Cliente actualizado exitosamente',
+                position: 'top-right',
+                icon: success ? successIcon : circleXMarkIcon,
+                duration: 3000,
+            }))
+            clearProcessMessageBranch()
+        }
+
+
+        if (success) dispatch(triggerReload()); dispatch(setSuccessBranch(false))
+
+    }, [message, success])
+
+    useEffect(() => {
+        if (info) handleCreateBranch()
+    }, [info])
+
+    return (
+        <div className='flex justify-center'>
+            {
+                clients && <Inputs inputs={inputs} setInfo={setInfo} styles={styles} submitting={loading} />
+            }
+        </div>
+    )
+}

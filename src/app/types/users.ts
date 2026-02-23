5ada46feb55d7c4bfd8b4d32f04ccf47e207ae9b
Dario Moreno <dario_moreno10@hotmail.com>
2026-02-23 16:25:56 -0500

    Feat - Se crea interface para el mapeo de datos del componente Usuarios

---
+++ src/app/types/users.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id?: number;
+  identifier: number;
+  name: string;
+  lastname: string;
+  email: string;
+  avatar_url?: null;
+  status: number;
+  identification_types_id: number;
+  roles_ids: number[];
+  subscribers_id?: number;
+}

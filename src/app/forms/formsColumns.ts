1b1db9a5431d3bb1ef942a856c4ee68622b77aaf
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-04 10:38:05 -0500

    feature Se crea componente principal de formularios

---
+++ src/app/forms/formsColumns.ts
@@ -0,0 +1,15 @@
+import { Form } from "../types/forms";
+import { Column } from "../types/table";
+
+export const forms_columns: Column<Form>[] = [
+    {
+        accessorKey: 'name',
+        header: 'Nombre',
+        size: 200
+    },
+    {
+        accessorKey: 'description',
+        header: 'Descripción',
+        size: 300
+    }
+]
---
+++ src/app/forms/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Forms from './forms'
+
+export default function page() {
+    return (
+        <Forms />
+    )
+}

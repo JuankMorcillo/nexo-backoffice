a87ab5ec58e200e78854d1d07e0c9366fcb9633b
Dario Moreno <dario_moreno10@hotmail.com>
2026-03-05 16:38:35 -0500

    Feat - Se crea modulo para llegar al enpoint de actualizacion y consulta por ID de permisos

--- src/app/store/slices/PermissionSlice.ts
+++ src/app/store/slices/PermissionSlice.ts
@@ -1,4 +1,4 @@
-import { getPermissions, createPermission } from "@/src/lib/api/permissions";
+import { getPermissions, createPermission, getPermissionById, editPermission } from "@/src/lib/api/permissions";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { Permission } from "../../types/permissions";
 
@@ -14,7 +14,7 @@ const initialState = {
   },
   loading: false,
   message: '',
-  processMessageEqu: '',
+  processMessagePermission: '',
   success: false,
 }
 
@@ -24,6 +24,22 @@ export const fetchPermissions = createAsyncThunk<any, FetchPayload, { rejectValu
 
     try {
       const response = await getPermissions(token, params);
+
+      return response;
+    } catch (error: any) {
+      const errorMessage = error || 'Error desconocido';
+      return rejectWithValue(errorMessage);
+    }
+
+  }
+)
+
+export const fetchPermissionById = createAsyncThunk<any, { token: string; id: number }, { rejectValue: string }>(
+  'permissions/fetchPermissionById',
+  async ({ token, id }, { rejectWithValue }) => {
+
+    try {
+      const response = await getPermissionById(token, id);
 
       return response;
     } catch (error: any) {
@@ -50,6 +66,24 @@ export const createPermissionSlice = createAsyncThunk<any, { permission: Permiss
   }
 )
 
+
+export const editPermissionSlice = createAsyncThunk<any, { permission: Permission, token: string }, { rejectValue: string }>(
+  'permissions/ediPermission',
+  async (permission, { rejectWithValue }) => {
+
+    try {
+
+      const result = await editPermission(permission.token || '', permission.permission);
+
+      return result;
+    } catch (error: any) {
+      const errorMessage = error || 'Error desconocido';
+      return rejectWithValue(errorMessage);
+    }
+
+  }
+)
+
 const permissionSlice = createSlice({
   name: 'permissions',
   initialState,
@@ -61,7 +95,7 @@ const permissionSlice = createSlice({
       state.message = '';
     },
     clearProcessMessagePermission(state) {
-      state.processMessageEqu = '';
+      state.processMessagePermission = '';
     },
     setSuccessPermission(state, action) {
       state.success = action.payload;
@@ -84,20 +118,53 @@ const permissionSlice = createSlice({
         state.message = action.payload || 'Error al cargar los Permisos';
       })
 
+    // Fetch Permission By Id
+    builder
+      .addCase(fetchPermissionById.pending, (state) => {
+        state.loading = true;
+        state.message = '';
+      })
+      .addCase(fetchPermissionById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.permissions = action.payload.data || [];
+        state.total = action.payload.total || 0;
+      })
+      .addCase(fetchPermissionById.rejected, (state, action) => {
+        state.loading = false;
+        state.message = action.payload || 'Error al cargar el Permiso';
+      })
+
     // Create Permission
     builder
       .addCase(createPermissionSlice.pending, (state) => {
         state.loading = true;
-        state.processMessageEqu = '';
+        state.processMessagePermission = '';
       })
       .addCase(createPermissionSlice.fulfilled, (state, action) => {
         state.loading = false;
-        state.processMessageEqu = action.payload.message || 'Permiso creado exitosamente';
+        state.processMessagePermission = action.payload.message || 'Permiso creado exitosamente';
         state.success = true;
       })
       .addCase(createPermissionSlice.rejected, (state, action) => {
         state.loading = false;
-        state.processMessageEqu = action.payload || 'Error al crear el Permis';
+        state.processMessagePermission = action.payload || 'Error al crear el Permiso';
+        state.success = false;
+      })
+
+    // Edit Permission
+    builder
+      .addCase(editPermissionSlice.pending, (state) => {
+        state.loading = true;
+        state.processMessagePermission = '';
+      })
+      .addCase(editPermissionSlice.fulfilled, (state, action) => {
+        state.loading = false;
+        state.processMessagePermission = action.payload.message || 'Permiso editado exitosamente';
+        state.success = true;
+      })
+      .addCase(editPermissionSlice.rejected, (state, action) => {
+        state.loading = false;
+        state.processMessagePermission = action.payload || 'Error al editar el Permiso';
         state.success = false;
       })
 
@@ -109,6 +176,6 @@ export const { setParams, clearMessage, clearProcessMessagePermission, setSucces
 export const selectPermissionLoading = (state: any) => state.permissions.loading;
 export const selectPermissionSuccess = (state: any) => state.permissions.success;
 export const selectPermissionMessage = (state: any) => state.permissions.message;
-export const selectPermissionProcessMessage = (state: any) => state.permissions.processMessageEqu;
+export const selectPermissionProcessMessage = (state: any) => state.permissions.processMessagePermission;
 
 export default permissionSlice.reducer;
--- src/lib/api/permissions.ts
+++ src/lib/api/permissions.ts
@@ -21,12 +21,51 @@ export async function getPermissions(token: string, params = {}) {
   }
 }
 
+export async function getPermissionById(token: string, id: number) {
+
+    const options: OptionsProps = {
+        endpoint: `permissions/${id}`,
+        method: 'GET' as const,
+        headers: {
+            Authorization: 'bearer ' + token,
+        }
+    }
+
+    try {
+        return await FetchApi(options);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+}
 
 export async function createPermission(token: string, data: Permission) {
 
     const options: OptionsProps = {
         endpoint: 'permissions',
         method: 'POST' as const,
+        headers: {
+            Authorization: 'bearer ' + token,
+        },
+        body: data
+    }
+
+    try {
+        return await FetchApi(options);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+
+}
+
+
+export async function editPermission(token: string, data: Permission) {
+
+    const options: OptionsProps = {
+        endpoint: `permissions/${data.id}`,
+        method: 'PATCH' as const,
         headers: {
             Authorization: 'bearer ' + token,
         },

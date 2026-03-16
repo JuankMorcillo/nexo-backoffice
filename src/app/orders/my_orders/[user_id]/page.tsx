03a9f1f6e6b9bfc6946d4ff10e470908b124dc83
@juankmorcillo2 <juankmorcillo2@gmail.com>
2026-03-16 16:53:49 -0500

    feature Se crea componente para ver las ordenes del usuario; se agrega icono

---
+++ src/app/orders/my_orders/[user_id]/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import User_Orders from './user_orders'
+import { Metadata } from 'next';
+
+export async function generateMetadata({ params }: { params: { user_id: string } }): Promise<Metadata> {
+    return {
+        title: `Mis órdenes - Usuario ${params.user_id}`,
+        description: 'Visualiza tus órdenes pendientes',
+    };
+}
+
+export default function page() {
+    return (
+        <User_Orders />
+    )
+}
---
+++ src/app/orders/my_orders/[user_id]/user_orders.tsx
@@ -0,0 +1,103 @@
+'use client';
+
+import Iconos from '@/src/app/components/ui/hooks/iconos';
+import { AppDispatch } from '@/src/app/store'
+import { fetchOrdersUser, selectOrders } from '@/src/app/store/slices/ordersSlice'
+import { useSession } from 'next-auth/react'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+export default function User_Orders() {
+
+    const dispatch = useDispatch<AppDispatch>()
+    const { data: session } = useSession()
+    const orders = useSelector(selectOrders)
+
+    const { locationDotIcon } = Iconos({ classNames: 'size-6', fill: 'currentColor', stroke: 'currentColor', strokeWidth: 1.5 })
+
+    const fetchOrders = async () => {
+        if (session?.user?.access_token) {
+
+            try {
+
+                const response = await dispatch(
+                    fetchOrdersUser({ token: session.user.access_token })
+                )
+
+            } catch (error) {
+                console.error('Error fetching orders:', error);
+            }
+
+        }
+    }
+
+    useEffect(() => {
+        fetchOrders()
+    }, [session])
+
+
+    return (
+        <>
+
+            {
+                orders && orders.length > 0 ?
+                    <div className='flex flex-col w-full max-w-120'>
+                        <h2>Órdenes del usuario</h2>
+
+                        <div className='flex flex-col'>
+                            {
+                                orders.map((order: any) => {
+                                    const completedTasks = order.tasks.filter((task: any) => task.status == 4).length;
+                                    const totalTasks = order.tasks.length;
+                                    const progressPercentage = (completedTasks / totalTasks) * 100;
+                                    return (
+
+                                        <div key={order.id} className='flex flex-col bg-white p-4 mb-4 rounded-xl shadow-sm border-l-[6px] border-l-[#137fec]'>
+
+                                            <span className='font-bold'>
+                                                {order.ordersType.name}
+                                            </span>
+
+                                            <span className='flex flex-row text-sm text-gray-500 items-center gap-1'>
+                                                {locationDotIcon} {order.branch.name}
+                                            </span>
+
+                                            <div className='flex flex-col'>
+                                                <div className='flex justify-between items-center mt-2 mb-1'>
+                                                    <span className='text-sm text-gray-500'>
+                                                        Progreso de tareas
+                                                    </span>
+                                                    <span className='text-sm font-bold'>
+                                                        {completedTasks}/{totalTasks} Completed
+                                                    </span>
+                                                </div>
+                                                <div className="w-full bg-slate-100  h-1.5 rounded-full overflow-hidden">
+                                                    <div
+                                                        className="bg-red-500 h-full rounded-full"
+                                                        style={{
+                                                            width: `${progressPercentage}%`
+                                                        }}
+                                                    ></div>
+                                                </div>
+
+                                            </div>
+
+
+
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+
+
+                    </div>
+                    :
+                    <p>No se encontraron órdenes para este usuario.</p>
+
+            }
+
+        </>
+    )
+}
